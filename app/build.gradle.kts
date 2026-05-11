f6752cd77c70d13d8da8fbfe9ef1b2eba84413f5
Maxine Vagnes <contact@maxine.dev>
2026-05-11 21:50:14 +0200

    release: bump to versionCode 2, versionName 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.maxine.librelune"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
