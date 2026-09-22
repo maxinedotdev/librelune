c195e80851335162fac2271e9eb380c8841b65fa
Maxine Vagnes <contact@maxine.dev>
2026-09-22 13:40:12 +0200

    release: bump versionCode to 4 and versionName to 0.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.maxine.librelune"
         minSdk = 31
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.4.2"
+        versionCode = 4
+        versionName = "0.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
